53233e9b687a640b14e8f72f624855d042314487
Claudia Menting <klodya@gmail.com>
2016-11-18 11:45:07 +0000

    adding sql details

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,11 +1,20 @@
 require('pg')
+require('uri')
 
 class SqlRunner
 
   def self.run( sql )
-    begin
-      db = PG.connect({ dbname: 'palsapalooza', host: 'localhost' })
-      result = db.exec( sql )
+    
+    if ENV['DATABASE_URL'] == nil
+         uri = URI.parse("http://localhost/palsapalooza")
+       else
+         uri = URI.parse(ENV['DATABASE_URL'])    
+       end
+       
+       begin
+         db = PG.connect( dbname: uri.path[1..-1], host: uri.host, user: uri.user, password: uri.password)
+         result = db.exec ( sql )
+    
     ensure
       db.close
     end
